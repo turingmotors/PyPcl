5f555146bcbe40e7b0e14f3dfd817181b0d9b6ab
mats <kazuki.matsuda@turing-motors.com>
2024-04-09 00:27:34 +0900

    fix

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name="PyPcl",
+    name="pcl-python",
+    packages=["pypcl"],
     version="0.0.1",
     description="Point Cloud Library for Python",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author="hiroyuki shimatani",
-    packages=["pypcl"],
+    author="Hiroyuki Shimatani",
+    url="https://github.com/turingmotors/PyPcl",
     python_requires=">=3.11",
 )

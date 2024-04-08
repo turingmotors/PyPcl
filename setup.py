b094b67e65a35c97caf304c2bf8676e21ea0f1c3
mats <kazuki.matsuda@turing-motors.com>
2024-04-08 23:59:23 +0900

    add long_description

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+from pathlib import Path
+
 from skbuild import setup
+
+# read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="PyPcl",
     version="0.0.1",
-    description="minimal python wrapper for pcl",
+    description="Point Cloud Library for Python",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="hiroyuki shimatani",
     packages=["pypcl"],
     python_requires=">=3.11",

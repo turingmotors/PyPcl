8fcf64fb0b5fb0650dc8589d24f11fb10fa997f4
mats <kazuki.matsuda@turing-motors.com>
2024-04-08 16:33:15 +0000

    fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name="pcl-python",
+    name="pypcl",
     packages=["pypcl"],
     version="0.0.1",
     description="Point Cloud Library for Python",

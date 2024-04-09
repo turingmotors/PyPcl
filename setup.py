e5ea419849a200d6f6de5008e84825abf4c002e2
mats <kazuki.matsuda@turing-motors.com>
2024-04-09 02:07:51 +0000

    add classifiers

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
+import os
 from pathlib import Path
+import sys
 
 from skbuild import setup
 
 # read the contents of your README file
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
+
+# プラットフォームに応じてmanylinuxタグを設定する
+if sys.platform.startswith("linux"):
+    machine = os.uname().machine
+    if machine == "x86_64":
+        manylinux_tag = "manylinux2014_x86_64"
+    elif machine == "aarch64":
+        manylinux_tag = "manylinux2014_aarch64"
+    else:
+        raise ValueError(f"Unsupported machine: {machine}")
+else:
+    manylinux_tag = None
 
 setup(
     name="pcl-python",
@@ -16,4 +30,10 @@ setup(
     author="Hiroyuki Shimatani",
     url="https://github.com/turingmotors/PyPcl",
     python_requires=">=3.11",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        f"Platform :: {manylinux_tag}" if manylinux_tag else "",
+    ],
 )

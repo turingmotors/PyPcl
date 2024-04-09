b75740f6402cfa790a8ebb6bbac91229b22d1f33
mats <kazuki.matsuda@turing-motors.com>
2024-04-09 02:41:49 +0000

    fix classifiers

--- setup.py
+++ setup.py
@@ -8,32 +8,21 @@ from skbuild import setup
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-# プラットフォームに応じてmanylinuxタグを設定する
-if sys.platform.startswith("linux"):
-    machine = os.uname().machine
-    if machine == "x86_64":
-        manylinux_tag = "manylinux2014_x86_64"
-    elif machine == "aarch64":
-        manylinux_tag = "manylinux2014_aarch64"
-    else:
-        raise ValueError(f"Unsupported machine: {machine}")
-else:
-    manylinux_tag = None
-
 setup(
     name="pcl-python",
     packages=["pypcl"],
     version="0.0.2",
-    description="Point Cloud Library for Python",
+    description="Python binding for Point Cloud Library (PCL)",
     long_description=long_description,
     long_description_content_type='text/markdown',
     author="Hiroyuki Shimatani",
     url="https://github.com/turingmotors/PyPcl",
     python_requires=">=3.11",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: POSIX :: Linux",
-        f"Platform :: {manylinux_tag}" if manylinux_tag else "",
+        f"Architecture :: {os.uname().machine}",
     ],
 )
